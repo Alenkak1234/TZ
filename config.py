b6cabec8e71725bba5d8959735f06124164b87ad
Alenka_pelmeshka <95702273+Alenkak1234@users.noreply.github.com>
2023-10-22 23:50:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = '5009627012:AAG6QxHnC5MfygxI9DAcncX7d4KduVUzNkY'
-PAYMENTS_TOKEN = '401643678:TEST:d73ce9eb-f895-48c2-9d51-0304c06e2495'
+BOT_TOKEN = 'токен'
+PAYMENTS_TOKEN = 'токен'
 
 item_url = 'https://images.unsplash.com/photo-1603302576837-37561b2e2302?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1468&q=80'
